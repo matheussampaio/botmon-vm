4dd59412e1e61c1d3199cbb93c1664ba24fb0b36
Matheus Sampaio <matheus1401@gmail.com>
2016-08-08 19:37:33 -0500

    feat: coisa pa carai

--- src/index.js
+++ src/index.js
@@ -1,28 +1,50 @@
+const path = require('path');
 const spawn = require('child_process').spawn;
 const firebase = require('firebase');
 
 // Load config
-const config = require('../configs/config.json');
+const config = require(path.join(__dirname, '../configs/config.json'));
 
-if (!config.vm_id) {
+const CONFIG_VM_ID = process.env.CONFIG_VM_ID || config.vm_id;
+
+if (!CONFIG_VM_ID) {
   console.error('vm_id not found');
   process.exit(1);
 }
 
+console.log('vm dir', __dirname);
+
 firebase.initializeApp({
-  serviceAccount: './configs/meowth-config.json',
+  serviceAccount: path.join(__dirname, '../configs/meowth-config.json'),
   databaseURL: 'https://meowth-aed86.firebaseio.com'
 });
 
 const botsRef = firebase.database().ref('bots');
-const vmRef = firebase.database().ref('vms').child(config.vm_id);
+const vmRef = firebase.database().ref('vms').child(CONFIG_VM_ID);
 
 // heartbeat every 5 seconds
 const heartbeatInterval = setInterval(() => {
-  vmRef.update({
-    status: 'online',
-    timestamp: (new Date).getTime()
+  vmRef.once('value', snapshot => {
+    vm = snapshot.val();
+
+    if (vm.status === 'shut_down') {
+      console.log(`shuting down vm ${CONFIG_VM_ID}`);
+
+      vmRef.update({
+        status: 'offline',
+        timestamp: (new Date).getTime()
+      }, () => {
+        process.exit(0);
+      });
+
+    } else {
+      vmRef.update({
+        status: 'online',
+        timestamp: (new Date).getTime()
+      });
+    }
   });
+
 }, 5000);
 
 // listen for update on `/vms/<vm_id>/bots/`
@@ -33,9 +55,7 @@ function handleBot(key) {
   botsRef.child(key).once('value', snapshot => {
     const bot = snapshot.val();
 
-    console.log(key, bot);
-
-    if (bot.vm === config.vm_id && bot.status === 'waiting_for_vm') {
+    if (bot && bot.vm === CONFIG_VM_ID && bot.status === 'waiting_for_vm') {
       // update bot.status to 'starting_bot'
       botsRef.child(key).update({ status: 'starting_bot' });
 
